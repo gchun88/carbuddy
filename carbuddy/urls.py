f1d4c4cf1934c6eb919d1b77b5da3269485c7da6
Michael Lee <lee.cs.michael@gmail.com>
2020-07-15 22:24:26 -0700

    Implemented navigation bar

--- carbuddy/urls.py
+++ carbuddy/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.testlink1),
+    path('',views.index, name='index'),
     path('admin/', admin.site.urls),
     path('carmodels/',views.testlink2),
     path('carmodels/details/',views_listing.details),
--- carbuddy/views.py
+++ carbuddy/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
-def testlink1(request):
-    html = '<html><body><a href="carmodels/">carmodel List</a></body></html>'
-    return HttpResponse(html)
+
+def index(request):
+	#View function for home page of site
+	return render(request, 'index.html') 
+
+
+# def testlink1(request):
+#     html = '<html><body><a href="carmodels/">carmodel List</a></body></html>'
+#     return HttpResponse(html)
 
 
 
