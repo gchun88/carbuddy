a3b67f5682f0c77c7442cf22e96cfc2c8eb34a5d
tyler <gchun88@gmail.com>
2020-08-01 12:29:13 -0700

    Javascript practice

--- listing/views.py
+++ listing/views.py
@@ -12,9 +12,6 @@ def details(request):
 
 
 
-def details2(request):
-    heejae = 'Matilda'
-    return render(request,'listing/details2.html',{'a':heejae})
 
 
 class CarInstanceListView(generic.ListView):
